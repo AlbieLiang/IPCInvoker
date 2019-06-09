927bbb7da6f44814b2b59b40572c9960bd42f023
albieliang <albieliang@gmail.com>
2019-06-09 16:03:35 +0800

    chore: [rename] rename class name from `Monitor` to `Monitoring`

--- ipc-invoker/src/main/java/cc/suitalk/ipcinvoker/monitor/ConnectRemoteServiceMonitoring.java
+++ ipc-invoker/src/main/java/cc/suitalk/ipcinvoker/monitor/ConnectRemoteServiceMonitoring.java
@@ -3,5 +3,5 @@ package cc.suitalk.ipcinvoker.monitor;
 /**
  * Created by albieliang on 2019-05-25.
  */
-public class ConnectRemoteServiceMonitor {
+public interface ConnectRemoteServiceMonitoring {
 }
